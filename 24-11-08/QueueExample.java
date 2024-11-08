e043a85fb2cbe7833c56171a722c67002b167caf
Lukas Norlund <johan.lukas@hotmail.com>
2024-11-08 11:24:53 +0900

    Added QueueExample

---
+++ 24-11-08/QueueExample.java
@@ -0,0 +1,22 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class QueueExample {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<String>();
+
+        queue.offer("(1)");
+        System.out.println("キューの状態:" + queue);
+        queue.offer("(2)");
+        System.out.println("キューの状態:" + queue);
+        queue.offer("(3)");
+        System.out.println("キューの状態:" + queue);
+        queue.offer("(3)");
+        System.out.println("キューの状態:" + queue);
+
+        while (!queue.isEmpty()) {
+            System.out.println("要素の取り出し：" + queue.poll());
+            System.out.println("キューの状態：" + queue);
+        }
+    }
+}
