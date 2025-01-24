4815a64c9f10b9b6ffe49765ee4847b3ce469906
lukkeswe <johan.lukas@hotmail.com>
2025-01-24 12:05:16 +0900

    Added WeatherForcastServer

--- 25-01-24/WeatherForcastServer.java
+++ 25-01-24/WeatherForcastServer.java
@@ -1,5 +1,26 @@
-
+import java.io.*;
+import java.net.*;
 
 public class WeatherForcastServer {
-    
+    static String[] weathers = {"晴れ", "曇り", "雨", "晴れのち曇り", "晴れのち雨", "曇りのち晴れ"};
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5001);
+            while(true){
+                Socket socket = serverSocket.accept();
+                PrintWriter writer = new PrintWriter(socket.getOutputStream());
+
+                String weather = getWeatherForcast();
+                writer.println(weather);
+                writer.close();
+                System.out.println("[" + weather + "] の予報を送りました");
+            }
+        } catch(IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    static String getWeatherForcast() {
+        return weathers[(int)(Math.random() * weathers.length)];
+    }
 }
